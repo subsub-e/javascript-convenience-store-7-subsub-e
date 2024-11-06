a2ccb7efde9abb8eb08285fc9d0d2c3432f795b3
subsub-e <ab13696802@gmail.com>
2024-11-06 17:02:48 +0900

    feat(errorConstants) : 에러 코드 및 에러 상수화

---
+++ src/constants/errorConstants.js
@@ -0,0 +1,10 @@
+const ERROR_MESSAGES = Object.freeze({
+  INVALID_PURCHASE_FORMAT:
+    '[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.',
+  PRODUCT_NOT_FOUND: '[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.',
+  OUT_OF_STOCK_LIMIT:
+    '[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.',
+  INVALID_INPUT: '[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.',
+});
+
+export default ERROR_MESSAGES;
