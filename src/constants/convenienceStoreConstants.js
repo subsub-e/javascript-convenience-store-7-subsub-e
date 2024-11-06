0c286c592464cb3eb3f05b7039d8ec8af49b623e
subsub-e <ab13696802@gmail.com>
2024-11-06 17:04:02 +0900

    feat(convenienceStoreConstants) : 편의점 서비스에 필요한 상수 정의

---
+++ src/constants/convenienceStoreConstants.js
@@ -0,0 +1,10 @@
+const CONVENIENCE_STORE_CONSTANTS = Object.freeze({
+  ANSWER_YES: 'Y',
+  ANSWER_NO: 'N',
+  MONEY_UNIT: '원',
+  OUT_OF_STOCK: '재고 없음',
+  QUANTITY: '개',
+  STOCK_IS_ZERO: 0,
+});
+
+export default CONVENIENCE_STORE_CONSTANTS;
