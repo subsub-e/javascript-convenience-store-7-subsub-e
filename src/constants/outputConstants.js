5fad88d840e28958e4e9d711a5b820f1d4031f65
subsub-e <ab13696802@gmail.com>
2024-11-06 17:03:30 +0900

    feat(outputConstants) : 편의점 서비스 출력 메세지 상수화

---
+++ src/constants/outputConstants.js
@@ -0,0 +1,16 @@
+const OUTPUT_MESSAGES = Object.freeze({
+  DASH: '-',
+  Greetings: '안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n',
+  RECEIPT_HEADER: '\n===========W 편의점=============\n',
+  GOODS: '상품명',
+  AMOUNT: '수량',
+  PRICE: '금액',
+  PROMOTION_INFO: '===========증	  정=============\n',
+  DIVISION_LINE: '==============================\n',
+  TOTAL_PURCHASE_AMOUNT: '총구매액',
+  EVENT_DISCOUNT: '행사할인',
+  MEMBERSHIP_DISCOUNT: '멤버십할인',
+  MONEY_TO_PAY: '내실돈',
+});
+
+export default OUTPUT_MESSAGES;
