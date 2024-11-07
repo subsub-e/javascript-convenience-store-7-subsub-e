0e13bad1840e1e0a08d48f742a52cddb8f08ea8c
subsub-e <ab13696802@gmail.com>
2024-11-07 22:00:39 +0900

    feat(InputService) : 잘못된 값 입력시 다시 입력 받는 InputService 구현

---
+++ src/services/InputService.js
@@ -0,0 +1,71 @@
+import InputView from '../views/InputView.js';
+import OutputView from '../views/OutputView.js';
+import { validatePurchaseInfo, validateAnswer } from '../utils/validation.js';
+import { formatPurchaseInfo } from '../utils/formatting.js';
+
+class InputService {
+  async #readInputUntilValid(
+    inputMethod,
+    validationFunction,
+    isGetPurchaseInfo = null
+  ) {
+    const input = await inputMethod();
+    try {
+      validationFunction(input);
+      if (isGetPurchaseInfo) {
+        return isGetPurchaseInfo(input);
+      }
+      return input;
+    } catch (error) {
+      OutputView.printErrorMessage(error.message);
+      return this.#readInputUntilValid(
+        inputMethod,
+        validationFunction,
+        isGetPurchaseInfo
+      );
+    }
+  }
+
+  async getPurchaseInfo(stock) {
+    return this.#readInputUntilValid(
+      InputView.readPurchaseItemAndQuantity,
+      validatePurchaseInfo,
+      (input) => {
+        const formattedPurchaseInfo = formatPurchaseInfo(input);
+        stock.checkPurchaseInfo(formattedPurchaseInfo);
+        stock.checkOutOfStock(formattedPurchaseInfo);
+        return formattedPurchaseInfo;
+      }
+    );
+  }
+
+  async askForMembershipDiscount() {
+    return this.#readInputUntilValid(
+      InputView.readMembershipDiscount,
+      validateAnswer
+    );
+  }
+
+  async askToAddPromotionQuantity() {
+    return this.#readInputUntilValid(
+      InputView.readAddQuantityDecision,
+      validateAnswer
+    );
+  }
+
+  async askToPayNonPromotionItem() {
+    return this.#readInputUntilValid(
+      InputView.readPayNonPromotionItem,
+      validateAnswer
+    );
+  }
+
+  async askForMorePurchase() {
+    return this.#readInputUntilValid(
+      InputView.readMorePurchase,
+      validateAnswer
+    );
+  }
+}
+
+export default InputService;
