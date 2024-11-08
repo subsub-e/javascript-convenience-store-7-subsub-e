638b46950cf4a8fea785f5b3165f74a3c5467bc7
subsub-e <ab13696802@gmail.com>
2024-11-08 19:08:06 +0900

    feat(Stock) : Stock 메소드 수정 및 재고를 뺴는 메소드 추가

--- src/models/Stock.js
+++ src/models/Stock.js
@@ -13,29 +13,56 @@ class Stock {
     return this.#productInfo;
   }
 
+  getProductTotalPrice(productName, quantity) {
+    const product = this.findProductsInStock(productName);
+    return product[0].price * quantity;
+  }
+
   checkPurchaseInfo(purchaseInfo) {
     const missingProducts = Object.keys(purchaseInfo).filter((productName) => {
-      return !this.#findProductInStock(productName);
+      return this.findProductsInStock(productName).length === 0;
     });
 
     if (missingProducts.length > 0) {
-        throw new Error(ERROR_MESSAGES.PRODUCT_NOT_FOUND);
+      throw new Error(ERROR_MESSAGES.PRODUCT_NOT_FOUND);
     }
   }
 
   checkOutOfStock(purchaseInfo) {
-    const outOfStockProducts = Object.keys(purchaseInfo).filter((productName) => {
-      const product = this.#findProductInStock(productName);
-      return product && purchaseInfo[productName] > product.quantity;
-    });
+    const outOfStockProducts = Object.keys(purchaseInfo).filter(
+      (productName) => {
+        const products = this.findProductsInStock(productName);
+
+        let totalQuantityAvailable = products.reduce(
+          (sum, product) => sum + product.quantity,
+          0
+        );
+
+        return purchaseInfo[productName] > totalQuantityAvailable;
+      }
+    );
 
     if (outOfStockProducts.length > 0) {
-        throw new Error(ERROR_MESSAGES.OUT_OF_STOCK_LIMIT);
+      throw new Error(ERROR_MESSAGES.OUT_OF_STOCK_LIMIT);
     }
   }
 
-  #findProductInStock(name) {
-    return this.#productInfo.find((product) => product.name === name);
+  findProductsInStock(name) {
+    return this.#productInfo.filter((product) => product.name === name);
+  }
+
+  findProductSameNameAndPromotion(product) {
+    return this.#productInfo.find((item) => item.name === product.name && item.promotion === product.promotion);
+  }
+
+  subtrackStock(product, quantity) {
+    const productInStock = this.findProductSameNameAndPromotion(product);
+    productInStock.quantity -= quantity;
+  }
+
+  additionalSubtrackStock(productName, quantity) {
+    const productInStock = this.findProductSameNameAndPromotion({ name: productName, promotion: 'null' });
+    productInStock.quantity -= quantity;
   }
 
   #parseFileData(fileData) {
