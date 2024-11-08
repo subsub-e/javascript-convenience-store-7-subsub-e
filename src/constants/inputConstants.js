f406424b554fb2a3ab4e298c3ff55a8d31eee3a6
subsub-e <ab13696802@gmail.com>
2024-11-08 19:08:54 +0900

    fix(Constants) : inputConstants, outputConstants 잘못된 개행 문자 수정

--- src/constants/inputConstants.js
+++ src/constants/inputConstants.js
@@ -1,11 +1,11 @@
 const INPUT_MESSAGES = Object.freeze({
   PURCHASE_ITEM_AND_QUANTITY:
     '구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n',
-  MEMBERSHIP_DISCOUNT: '멤버십 할인을 받으시겠습니까? (Y/N)\n',
-  MORE_PURCHASE: '감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)\n',
+  MEMBERSHIP_DISCOUNT: '\n멤버십 할인을 받으시겠습니까? (Y/N)\n',
+  MORE_PURCHASE: '\n감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)\n',
   PAY_NONPROMOTION_ITEM:
     '개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n',
-  NOW: '현재 ',
+  NOW: '\n현재 ',
   ADD_PROMOTION_QUANTITY:
     '은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n',
 });
