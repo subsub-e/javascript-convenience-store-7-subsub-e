5f54b712bde524a142ef555e051ced11289296ca
subsub-e <ab13696802@gmail.com>
2024-11-08 19:08:26 +0900

    feat(Stock) : Stock 클래스 리팩토링에 따른 테스트 케이스 추가

--- __tests__/StockTest.js
+++ __tests__/StockTest.js
@@ -47,13 +47,45 @@ name,price,quantity,promotion
     }
   );
 
-  test.each([[{ 콜라: 20, 물: 1 }], [{ 콜라: 20 }]])(
+  test.each([[{ 콜라: 25, 물: 1 }], [{ 콜라: 25 }]])(
     'checkOutOfStock 메소드 테스트: 재고 수량을 초과하여 구매할 경우 에러를 발생시킨다.',
     (purchaseInfo) => {
       const stock = new Stock();
       expect(() => stock.checkOutOfStock(purchaseInfo)).toThrowError(
         ERROR_MESSAGES.OUT_OF_STOCK_LIMIT
       );
+    }
+  );
+
+  test.each([
+    [{ name: '콜라', promotion: '탄산2+1' }, 3, 7],
+    [{ name: '사이다', promotion: '탄산2+1' }, 5, 3],
+    [{ name: '콜라', promotion: 'null' }, 2, 8],
+  ])(
+    'subtrackStock 메소드 테스트: 특정 상품의 재고 수량을 감소시킨다.',
+    (product, quantity, expectedQuantity) => {
+      const stock = new Stock();
+  
+      stock.subtrackStock(product, quantity);
+  
+      const updatedProduct = stock.findProductSameNameAndPromotion(product);
+      expect(updatedProduct.quantity).toBe(expectedQuantity);
+    }
+  );
+
+  test.each([
+    ['콜라', 5, 5],
+    ['사이다', 3, 4],
+    ['물', 2, 8],
+  ])(
+    'additionalSubtrackStock 메소드 테스트: promotion이 null인 상품의 재고 수량을 감소시킨다.',
+    (productName, quantity, expectedQuantity) => {
+      const stock = new Stock();
+
+      stock.additionalSubtrackStock(productName, quantity);
+  
+      const productInStock = stock.findProductsInStock(productName).find(product => product.promotion === 'null');
+      expect(productInStock.quantity).toBe(expectedQuantity);
     }
   );
 
