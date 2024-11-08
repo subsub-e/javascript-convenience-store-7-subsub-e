aca8928265aaf7532bb950fd430db59a9abd22cc
subsub-e <ab13696802@gmail.com>
2024-11-08 20:31:24 +0900

    feat(Receipt) : 결과를 가지고 있는 Receipt 모델 구현

---
+++ src/models/Receipt.js
@@ -0,0 +1,71 @@
+class Receipt {
+  #purchaseList;
+  #promotionList;
+  #totalPurchasePrice;
+  #totalPromotionPrice;
+  #membershipDiscount;
+
+  constructor() {
+    this.#purchaseList = [];
+    this.#promotionList = [];
+    this.#totalPurchasePrice = 0;
+    this.#totalPromotionPrice = 0;
+    this.#membershipDiscount = 0;
+  }
+
+  addPurchaseInfo(productName, quantity, totalPrice) {
+    this.#purchaseList.push({
+      name: productName,
+      quantity: quantity,
+      totalPrice: totalPrice,
+    });
+    this.#totalPurchasePrice += totalPrice;
+    this.#membershipDiscount += totalPrice;
+  }
+
+  addPromotionInfo(
+    promotionAppliedProductName,
+    promotionAppliedCount,
+    totalPromotionDiscountPrice,
+    totalPrice
+  ) {
+    this.#promotionList.push({
+      name: promotionAppliedProductName,
+      quantity: promotionAppliedCount,
+    });
+    this.#totalPromotionPrice += totalPromotionDiscountPrice;
+    this.#membershipDiscount -= totalPrice;
+  }
+
+  getReceipt() {
+    return {
+      purchaseList: this.#purchaseList,
+      promotionList: this.#promotionList,
+      totalPurchasePrice: this.#totalPurchasePrice,
+      totalPromotionPrice: this.#totalPromotionPrice,
+      membershipDiscount: this.#calculateMembershipDiscount(
+        this.#membershipDiscount
+      ),
+      totalReturn: this.#calculateTotalReturn(),
+    };
+  }
+
+  #calculateMembershipDiscount(membershipDiscount) {
+    const calculatedDiscount = membershipDiscount * 0.3;
+
+    if (calculatedDiscount > 8000) {
+      return 8000;
+    }
+    return calculatedDiscount;
+  }
+
+  #calculateTotalReturn() {
+    return (
+      this.#totalPurchasePrice -
+      this.#totalPromotionPrice -
+      this.#calculateMembershipDiscount(this.#membershipDiscount)
+    );
+  }
+}
+
+export default Receipt;
