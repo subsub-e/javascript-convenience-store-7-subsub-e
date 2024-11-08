b4fa67a43bed8b7d0579675673f17ecb20f81245
subsub-e <ab13696802@gmail.com>
2024-11-08 19:11:55 +0900

    feat(formatting) : formatDate 메소드 추가

--- src/utils/formatting.js
+++ src/utils/formatting.js
@@ -30,6 +30,14 @@ export const formatPurchaseInfo = (inputPurchaseInfo) => {
   return result;
 };
 
+export const formatDate = (date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  
+  return `${year}-${month}-${day}`;
+}
+
 const parseInputPurchaseInfo = (inputPurchaseInfo) => {
   if (inputPurchaseInfo.indexOf(',') != -1) {
     return inputPurchaseInfo.split(',').map((product) => product.slice(1, -1));
