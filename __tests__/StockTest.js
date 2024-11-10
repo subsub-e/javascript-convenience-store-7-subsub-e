cf61946fbaaca7d051ef8046bc2200dcb9dd73c0
subsub-e <ab13696802@gmail.com>
2024-11-10 20:57:15 +0900

    feat(StockTest) : Stock 모델 리팩토링에 따른 테스트 코드 수정

--- __tests__/StockTest.js
+++ __tests__/StockTest.js
@@ -31,7 +31,9 @@ name,price,quantity,promotion
       { name: '사이다', price: 1000, quantity: 8, promotion: '탄산2+1' },
       { name: '사이다', price: 1000, quantity: 7, promotion: 'null' },
       { name: '오렌지주스', price: 1800, quantity: 9, promotion: 'MD추천상품' },
+      { name: '오렌지주스', price: 1800, quantity: 0, promotion: 'null' },
       { name: '탄산수', price: 1200, quantity: 5, promotion: '탄산2+1' },
+      { name: '탄산수', price: 1200, quantity: 0, promotion: 'null' },
       { name: '물', price: 500, quantity: 10, promotion: 'null' },
       { name: '비타민워터', price: 1500, quantity: 6, promotion: 'null' },
     ]);
