022c33b8f26dd10d135c24a0b8a18a903ddd8308
subsub-e <ab13696802@gmail.com>
2024-11-11 02:42:30 +0900

    fix(InputService) : 메소드 분리 리팩토링

--- src/services/InputService.js
+++ src/services/InputService.js
@@ -1,67 +1,83 @@
-import InputView from '../views/InputView.js';
-import OutputView from '../views/OutputView.js';
-import { validatePurchaseInfo, validateAnswer } from '../utils/validation.js';
-import { formatPurchaseInfo } from '../utils/formatting.js';
+import InputView from "../views/InputView.js";
+import OutputView from "../views/OutputView.js";
+import { validatePurchaseInfo, validateAnswer } from "../utils/validation.js";
+import { formatPurchaseInfo } from "../utils/formatting.js";
 
 class InputService {
-  async #readInputUntilValid(
-    inputMethod,
-    validationFunction,
-    isGetPurchaseInfo = null
-  ) {
+  async #readInputUntilValidWithoutParams(inputMethod, validationFunction) {
     const input = await inputMethod();
     try {
       validationFunction(input);
-      if (isGetPurchaseInfo) {
-        return isGetPurchaseInfo(input);
-      }
       return input;
     } catch (error) {
       OutputView.printErrorMessage(error.message);
-      return this.#readInputUntilValid(
+      return this.#readInputUntilValidWithoutParams(
         inputMethod,
-        validationFunction,
-        isGetPurchaseInfo
+        validationFunction
       );
     }
   }
 
+  async #readInputUntilValidWithParams(inputMethod, validationFunction, param) {
+    const input = await inputMethod(param);
+    try {
+      validationFunction(input);
+      return input;
+    } catch (error) {
+      OutputView.printErrorMessage(error.message);
+      return this.#readInputUntilValidWithParams(
+        inputMethod,
+        validationFunction,
+        param
+      );
+    }
+  }
+
+  #formatAndValidatePurchaseInfo(input, stock) {
+    try {
+      const formattedPurchaseInfo = formatPurchaseInfo(input);
+      stock.checkPurchaseInfo(formattedPurchaseInfo);
+      stock.checkOutOfStock(formattedPurchaseInfo);
+      return formattedPurchaseInfo;
+    } catch (error) {
+      OutputView.printErrorMessage(error.message);
+      return this.getPurchaseInfo(stock);
+    }
+  }
+
   async getPurchaseInfo(stock) {
-    return this.#readInputUntilValid(
+    const input = await this.#readInputUntilValidWithoutParams(
       InputView.readPurchaseItemAndQuantity,
-      validatePurchaseInfo,
-      (input) => {
-        const formattedPurchaseInfo = formatPurchaseInfo(input);
-        stock.checkPurchaseInfo(formattedPurchaseInfo);
-        stock.checkOutOfStock(formattedPurchaseInfo);
-        return formattedPurchaseInfo;
-      }
+      validatePurchaseInfo
     );
+    return this.#formatAndValidatePurchaseInfo(input, stock);
   }
 
   async askForMembershipDiscount() {
-    return this.#readInputUntilValid(
+    return this.#readInputUntilValidWithoutParams(
       InputView.readMembershipDiscount,
       validateAnswer
     );
   }
 
   async askToAddPromotionQuantity(productName) {
-    return this.#readInputUntilValid(
-      () => InputView.readAddQuantityDecision(productName),
-      validateAnswer
+    return this.#readInputUntilValidWithParams(
+      InputView.readAddQuantityDecision,
+      validateAnswer,
+      productName
     );
   }
 
   async askToPayNonPromotionItem(nonPromotionProductInfo) {
-    return this.#readInputUntilValid(
-      () => InputView.readPayNonPromotionItem(nonPromotionProductInfo),
-      validateAnswer
+    return this.#readInputUntilValidWithParams(
+      InputView.readPayNonPromotionItem,
+      validateAnswer,
+      nonPromotionProductInfo
     );
   }
 
   async askForMorePurchase() {
-    return this.#readInputUntilValid(
+    return this.#readInputUntilValidWithoutParams(
       InputView.readMorePurchase,
       validateAnswer
     );
