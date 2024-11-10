b1248839c097f6f9092ca8841b5ad3962dc65743
subsub-e <ab13696802@gmail.com>
2024-11-11 02:40:24 +0900

    fix(validation) : 입력 중복 체크 기능 추가 및 테스트 코드 추가

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,34 +1,40 @@
-import ERROR_MESSAGES from '../constants/errorConstants.js';
+import ERROR_MESSAGES from "../constants/errorConstants.js";
 
 export const validateAnswer = (answer) => {
-  if (answer !== 'Y' && answer !== 'N') {
+  if (answer !== "Y" && answer !== "N") {
     throw new Error(ERROR_MESSAGES.INVALID_INPUT);
   }
 };
 
 export const validatePurchaseInfo = (inputPurchaseInfo) => {
-  const purchaseItems = inputPurchaseInfo.split(',');
+  const purchaseItems = inputPurchaseInfo.split(",");
+  const itemSet = new Set();
   purchaseItems.forEach((item) => {
-    validateItem(item);
+    validateItem(item, itemSet);
   });
 };
 
-const validateItem = (item) => {
+const validateItem = (item, itemSet) => {
   isValidPurchaseFormat(item);
   const { name, quantity } = extractNameAndQuantity(item);
+  if (itemSet.has(name)) {
+    throw new Error(ERROR_MESSAGES.INVALID_PURCHASE_FORMAT);
+  }
   isValidName(name);
   isValidQuantity(quantity);
+  itemSet.add(name);
 };
 
+
 const isValidPurchaseFormat = (item) => {
-  if (!item.startsWith('[') || !item.endsWith(']')) {
+  if (!item.startsWith("[") || !item.endsWith("]")) {
     throw new Error(ERROR_MESSAGES.INVALID_PURCHASE_FORMAT);
   }
 };
 
 const extractNameAndQuantity = (item) => {
   const parseItem = item.slice(1, -1);
-  const lastDashIndex = parseItem.lastIndexOf('-');
+  const lastDashIndex = parseItem.lastIndexOf("-");
   return {
     name: parseItem.slice(0, lastDashIndex),
     quantity: Number(parseItem.slice(lastDashIndex + 1)),
