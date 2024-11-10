040116aec7c8d71400fa504a20b323453fa2d79b
subsub-e <ab13696802@gmail.com>
2024-11-11 02:42:09 +0900

    fix(OutputView) : 출력 형식 수정

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,14 +1,14 @@
-import OUTPUT_MESSAGES from "../constants/outputConstants.js";
-import { Console } from "@woowacourse/mission-utils";
+import OUTPUT_MESSAGES from '../constants/outputConstants.js';
+import { Console } from '@woowacourse/mission-utils';
 import {
   formatPrice,
   formatQuantity,
   formatPromotion,
-} from "../utils/formatting.js";
+} from '../utils/formatting.js';
 
 class OutputView {
   static printNewLine() {
-    Console.print("");
+    Console.print('');
   }
 
   static printErrorMessage(errorMessage) {
@@ -25,7 +25,6 @@ class OutputView {
       const formattedPrice = formatPrice(price);
       const formattedQuantity = formatQuantity(quantity);
       const promotionMessage = formatPromotion(promotion);
-
       Console.print(
         `${OUTPUT_MESSAGES.DASH} ${name} ${formattedPrice} ${formattedQuantity}${promotionMessage}`
       );
@@ -36,7 +35,7 @@ class OutputView {
   static showReceipt(receipt) {
     const result = receipt.getReceipt();
     const totalPurchasePriceLength = this.calculateMaxPriceLength(result);
-    this.printReceiptHeader();
+    this.printReceiptHeader(totalPurchasePriceLength);
     result.purchaseList.forEach((item) =>
       this.printPurchaseItem(item, totalPurchasePriceLength)
     );
@@ -58,25 +57,27 @@ class OutputView {
     );
   }
 
-  static printReceiptHeader() {
+  static printReceiptHeader(totalPurchasePriceLength) {
     Console.print(OUTPUT_MESSAGES.RECEIPT_HEADER);
     Console.print(
-      `${OUTPUT_MESSAGES.GOODS.padEnd(14, " ")}${OUTPUT_MESSAGES.AMOUNT.padEnd(
-        7,
-        " "
+      `${OUTPUT_MESSAGES.GOODS.padEnd(14, ' ')}${OUTPUT_MESSAGES.AMOUNT.padEnd(
+        13 - totalPurchasePriceLength,
+        ' '
       )}${OUTPUT_MESSAGES.PRICE}`
     );
   }
 
   static printPurchaseItem(item, totalPurchasePriceLength) {
-    const itemName = item.name.padEnd(17 - item.name.length, " ");
-    const itemQuantity = item.quantity.toString().padEnd(15 - totalPurchasePriceLength," ");
+    const itemName = item.name.padEnd(17 - item.name.length, ' ');
+    const itemQuantity = item.quantity
+      .toString()
+      .padEnd(15 - totalPurchasePriceLength, ' ');
     const itemPrice = item.totalPrice.toLocaleString();
     Console.print(`${itemName}${itemQuantity}${itemPrice}`);
   }
 
   static printPromotionItem(item) {
-    const itemName = item.name.padEnd(17 - item.name.length, " ");
+    const itemName = item.name.padEnd(17 - item.name.length, ' ');
     const itemQuantity = item.quantity;
     Console.print(`${itemName}${itemQuantity}`);
   }
@@ -92,12 +93,12 @@ class OutputView {
     Console.print(
       `${OUTPUT_MESSAGES.TOTAL_PURCHASE_AMOUNT.padEnd(
         13,
-        " "
+        ' '
       )}${result.totalPurchaseAmount
         .toString()
         .padEnd(
           15 - totalPurchasePriceLength,
-          " "
+          ' '
         )}${result.totalPurchasePrice.toLocaleString()}`
     );
   }
@@ -106,7 +107,7 @@ class OutputView {
     Console.print(
       `${OUTPUT_MESSAGES.EVENT_DISCOUNT.padEnd(
         28 - totalPurchasePriceLength,
-        " "
+        ' '
       )}${OUTPUT_MESSAGES.DASH}${result.totalPromotionPrice.toLocaleString()}`
     );
   }
@@ -115,7 +116,7 @@ class OutputView {
     Console.print(
       `${OUTPUT_MESSAGES.MEMBERSHIP_DISCOUNT.padEnd(
         27 - totalPurchasePriceLength,
-        " "
+        ' '
       )}${OUTPUT_MESSAGES.DASH}${result.membershipDiscount.toLocaleString()}`
     );
   }
@@ -124,7 +125,7 @@ class OutputView {
     Console.print(
       `${OUTPUT_MESSAGES.MONEY_TO_PAY.padEnd(
         29 - result.totalReturn.toLocaleString().length,
-        " "
+        ' '
       )}${result.totalReturn.toLocaleString()}`
     );
   }
